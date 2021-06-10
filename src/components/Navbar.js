8e6d3930cfb7e393612b3c2ee1f5610a66c1243c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-10 20:19:10 +0200

    working navbar and sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,151 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+  Hidden,
+  Button,
+  Grid,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import {
+  Menu as MenuIcon,
+  AccountCircle,
+  ShoppingCart,
+} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+import logo from "../assets/images/logo-192x192.png";
+import { links } from "../utils/constants";
+import { Link } from "react-router-dom";
+import Sidebar from "./Sidebar";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const classes = useStyles();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = useCallback(() => {
+    setIsDrawerOpen(true);
+  }, [isDrawerOpen]);
+
+  const handleDrawerClose = useCallback(() => {
+    setIsDrawerOpen(false);
+  }, [isDrawerOpen]);
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            component={Link}
+            to="/"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <LogoImage src={logo} alt="ecommerce logo" />
+            eCommerce
+          </Typography>
+
+          <Hidden mdUp>
+            <IconButton aria-label="Open navigation" onClick={handleDrawerOpen}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+
+          <Hidden smDown>
+            <NavLinks>
+              {links.map((link) => {
+                const { id, text, url } = link;
+                return (
+                  <Link key={id} to={url} className={classes.navLink}>
+                    <Button
+                      size="large"
+                      classes={{
+                        text: classes.menuButtonText,
+                      }}
+                    >
+                      {text}
+                    </Button>
+                  </Link>
+                );
+              })}
+            </NavLinks>
+
+            <div>
+              <IconButton
+                component={Link}
+                to="/cart"
+                aria-label="Show cart items"
+                color="inherit"
+              >
+                <Badge badgeContent={2} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+
+              <IconButton
+                component={Link}
+                to="/"
+                aria-label="Log in"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+          </Hidden>
+        </Toolbar>
+      </AppBar>
+      <Sidebar
+        links={links}
+        anchor="right"
+        open={isDrawerOpen}
+        onClose={handleDrawerClose}
+      />
+    </>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    boxShadow: "none",
+    borderBottom: "1px solid rgba(0,0,0,0.12)",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+  },
+  navLink: {
+    textDecoration: "none",
+    margin: "0 1em",
+  },
+  menuButtonText: {
+    fontSize: theme.typography.body1.fontSize,
+    fontWeight: theme.typography.h6.fontWeight,
+  },
+}));
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+  height: 25px;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 export default Navbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import { Menu as MenuIcon, ShoppingCart } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = (props) => {
+  const { width, open, onClose, anchor, links, selectedItem, theme } = props;
+  const classes = useStyles();
+  return (
+    <Drawer variant="temporary" open={open} onClose={onClose} anchor={anchor}>
+      <Toolbar className={classes.drawerHeader}></Toolbar>
+      <List className={classes.drawerList}>
+        {links.map((link) => {
+          const { id, text, url } = link;
+          return (
+            <Link
+              key={id}
+              to={url}
+              className={classes.drawerLink}
+              onClick={onClose}
+            >
+              <ListItem button className={classes.drawerButton}>
+                <ListItemText
+                  primary={
+                    <Typography className={classes.drawerLinkText}>
+                      {text}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            </Link>
+          );
+        })}
+      </List>
+    </Drawer>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  drawerHeader: {
+    width: 200,
+  },
+  drawerLink: {
+    textDecoration: "none",
+    color: "inherit",
+    textTransform: "capitalize",
+  },
+  drawerLinkText: {
+    fontSize: "1.5rem",
+  },
+  drawerButton: {
+    paddingLeft: "2em",
+  },
+  drawerList: {
+    height: "100%",
+  },
+}));
 
 export default Sidebar;

a19b955f25b20d1ca7ecfb0a266f286e2468f935
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-12 15:16:35 +0200

    added ThemeProvider and CSSBaseline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StylesProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StylesProvider>,
+  document.getElementById("root")
+);

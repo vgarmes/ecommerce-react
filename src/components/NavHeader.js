b8fb64d79181082282d5fd7f1d59ac5723d5bfa6
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-12 16:14:16 +0200

    Navbar header moved to its own component

---
+++ src/components/NavHeader.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import styled from "styled-components";
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "../assets/images/logo-192x192.png";
+import { Link } from "react-router-dom";
+
+const NavHeader = () => {
+  const classes = useStyles();
+  return (
+    <Typography
+      component={Link}
+      to="/"
+      variant="h6"
+      className={classes.title}
+      color="inherit"
+    >
+      <LogoImage src={logo} alt="ecommerce logo" />
+      eCommerce
+    </Typography>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+  },
+}));
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+  height: 25px;
+`;
+
+export default NavHeader;

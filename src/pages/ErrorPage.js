98161a856f0a1064d24f44a966ede6616c945bd1
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-14 09:10:36 +0200

    restructured container

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Container, Box, Typography, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const ErrorPage = () => {
-  return <div>Error Page</div>;
+  const classes = useStyles();
+  return (
+    <Container className={classes.content}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <div className={classes.toolbar} />
+        <Typography variant="h1">404</Typography>
+        <Typography paragraph variant="h4">
+          Sorry, the page you tried cannot be found
+        </Typography>
+        <Link to="/" className={classes.link}>
+          <Button className={classes.homeButton} variant="outlined">
+            Back to menu
+          </Button>
+        </Link>
+      </Box>
+    </Container>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  content: {
+    height: "100vh",
+  },
+  link: {
+    textDecoration: "none",
+  },
+  homeButton: {
+    marginTop: "3em",
+  },
+}));
 
 export default ErrorPage;

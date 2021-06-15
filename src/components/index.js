2d0d87e9ae59255b6214f667c863e5442650d3b5
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-15 13:53:03 +0200

    added components imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,17 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
+import PageBreadcrumbs from "./PageBreadcrumbs";
+import NavbarGap from "./NavbarGap";
+import FeaturedProducts from "./FeaturedProducts";
+import Hero from "./Hero";
 
-export { Navbar, Footer, Sidebar };
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  PageBreadcrumbs,
+  NavbarGap,
+  FeaturedProducts,
+  Hero,
+};

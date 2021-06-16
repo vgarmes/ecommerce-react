196969d469a6e1dac637d6af75e120f64759d671
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-16 11:25:36 +0200

    added mockup api url for products

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -15,3 +15,8 @@ export const links = [
     url: "/products",
   },
 ];
+
+export const products_url = "https://course-api.com/react-store-products";
+
+export const single_product_url =
+  "https://course-api.com/react-store-single-product?id=";

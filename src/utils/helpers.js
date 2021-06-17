022495fac97960fd175b9cd03ea77a09abc0c12e
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-17 09:58:22 +0200

    price formatter function returns directly the result

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,6 @@
 export const formatPrice = (number) => {
-  const newNumber = Intl.NumberFormat("en-US", {
+  return Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
   }).format(number / 100);
-  return newNumber;
 };

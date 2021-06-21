1429868bef487d896b929caf959651719793a425
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-21 22:36:54 +0200

    removed products variable

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,66 +1,39 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Container, Typography } from "@material-ui/core";
 import { Product } from "./";
 import { makeStyles } from "@material-ui/core/styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "60",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/skwgyqrbfzhu6uyeh0gg/air-max-270-mens-shoe-KkLcGR.png",
-  },
-  {
-    id: 2,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "60",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/916e10d5-5583-4850-bf8c-8ee2eed26d72/air-zoom-pulse-sko-f1Q7rK.png",
-  },
-  {
-    id: 3,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "60",
-    image:
-      "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/b5b12aba-5c3c-4817-a829-7d5ef713cc5e/react-miler-mens-running-shoe-DgF6nr.png",
-  },
-  {
-    id: 4,
-    name: "Shoes",
-    description: "Running shoes",
-    price: "60",
-    image:
-      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/cde221ba-ec30-4eb4-ae30-43ac0d548968/cosmic-unity-basketball-shoe-96Zs98.png",
-  },
-];
+const products = [];
 
 const ProductList = () => {
   const classes = useStyles();
   return (
-    <main className={classes.content}>
-      <Grid container justify="center" spacing={4}>
-        {products.map((product) => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product {...product} />
-          </Grid>
-        ))}
-      </Grid>
-    </main>
+    <section>
+      <Typography
+        className="section-title"
+        variant="h3"
+        align="center"
+        gutterBottom
+      >
+        Featured Products
+      </Typography>
+      <Container className={classes.content}>
+        <Grid container spacing={4}>
+          {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Product {...product} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
+  root: {},
   content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+    padding: theme.spacing(4),
   },
 }));
 

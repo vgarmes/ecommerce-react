5438039a72420722f3a3caca5fc9ac99e875bc26
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-21 22:38:51 +0200

    created productimages component

---
+++ src/components/ProductImages.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import styled from "styled-components";
+
+const ProductImages = ({ images = [{ url: "" }] }) => {
+  const theme = useTheme();
+  const [mainIndex, setMainIndex] = useState(0);
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <ImageContainer theme={theme}>
+          <img
+            className="main"
+            src={images[mainIndex].url}
+            alt={images[mainIndex].filename}
+          />
+        </ImageContainer>
+      </Grid>
+      {images.map((image, index) => {
+        return (
+          <Grid item key={index} xs={2} onClick={() => setMainIndex(index)}>
+            <ImageContainer theme={theme}>
+              <img
+                className={index === mainIndex ? "gallery active" : "gallery"}
+                src={image.url}
+                alt={image.filename}
+              />
+            </ImageContainer>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+const ImageContainer = styled.div`
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    object-fit: cover;
+  }
+  .active {
+    border: 2px solid ${(props) => props.theme.palette.primary.light};
+  }
+  .main {
+    height: 400px;
+  }
+
+  .gallery {
+    height: 60px;
+  }
+`;
+
+export default ProductImages;

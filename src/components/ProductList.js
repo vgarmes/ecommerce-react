0a894ec68c5ae31e6d42660a3777b7e12c48f10b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-01 14:30:02 +0200

    added grid_view to context variables

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,40 +1,15 @@
 import React from "react";
+import { useFilterContext } from "../context/filter_context";
 import { Grid, Container, Typography } from "@material-ui/core";
-import { Product } from "./";
+import GridView from "./GridView";
 import { makeStyles } from "@material-ui/core/styles";
-
-const products = [];
 
 const ProductList = () => {
   const classes = useStyles();
-  return (
-    <section>
-      <Typography
-        className="section-title"
-        variant="h3"
-        align="center"
-        gutterBottom
-      >
-        Featured Products
-      </Typography>
-      <Container className={classes.content}>
-        <Grid container spacing={4}>
-          {products.map((product) => (
-            <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <Product {...product} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </section>
-  );
+  const { filtered_products: products, grid_view } = useFilterContext();
+  return <GridView products={products}></GridView>;
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {},
-  content: {
-    padding: theme.spacing(4),
-  },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 export default ProductList;

b16212924044fc324fec3c2fde316467cf1a06dd
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-01 13:20:16 +0200

    created filter context and reducer and added its provider to the app

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from "./App";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
 import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
 import theme from "./theme";
 import "./index.css";
 
 ReactDOM.render(
   <ProductsProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <FilterProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </FilterProvider>
   </ProductsProvider>,
   document.getElementById("root")
 );

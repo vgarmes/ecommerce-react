eb5b7cffb2d692753f91c87fa8d87783c239cb8b
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-01 14:16:05 +0200

    add products to provider and export context

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -20,10 +20,20 @@ const initialState = {
 const FilterContext = React.createContext();
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products });
+  }, [products]);
+
   return (
-    <FilterContext.Provider value="filter context">
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   );
+};
+
+export const useFilterContext = () => {
+  return useContext(FilterContext);
 };

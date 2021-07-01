dc229eb8a5550a43e791cb5fd4c159fa16e9f5f4
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-01 10:14:24 +0200

    created variant selector component

---
+++ src/components/VariantSelector.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import styled from "styled-components";
+
+const VariantSelector = ({ variants, variantIndex, setVariantIndex }) => {
+  const theme = useTheme();
+  const images = variants.map((variant) => variant.image);
+  return (
+    <Grid container spacing={1}>
+      {images.map((image, index) => {
+        return (
+          <Grid item key={index} xs={2} onClick={() => setVariantIndex(index)}>
+            <ImageGalleryContainer theme={theme}>
+              <img
+                className={
+                  index === variantIndex ? "gallery active" : "gallery"
+                }
+                src={image.file.url}
+                alt={image.title}
+              />
+            </ImageGalleryContainer>
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
+
+const ImageGalleryContainer = styled.div`
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    object-fit: cover;
+  }
+  .active {
+    border: 2px solid ${(props) => props.theme.palette.primary.light};
+  }
+  .main {
+    height: 400px;
+  }
+
+  .gallery {
+    height: 60px;
+  }
+`;
+
+export default VariantSelector;

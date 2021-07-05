2a31206e945df9e0bada14062d43076b2be67ea0
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-05 13:05:45 +0200

    conditional rendering of gridview and listview

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,12 +2,26 @@ import React from "react";
 import { useFilterContext } from "../context/filter_context";
 import { Grid, Container, Typography } from "@material-ui/core";
 import GridView from "./GridView";
+import ListView from "./ListView";
 import { makeStyles } from "@material-ui/core/styles";
 
 const ProductList = () => {
   const classes = useStyles();
   const { filtered_products: products, grid_view } = useFilterContext();
-  return <GridView products={products}></GridView>;
+
+  if (products.length < 1) {
+    return (
+      <Typography variant="h5">
+        Sorry, no products matched your search
+      </Typography>
+    );
+  }
+
+  if (grid_view) {
+    return <GridView products={products} />;
+  }
+
+  return <ListView products={products} />;
 };
 
 const useStyles = makeStyles((theme) => ({}));

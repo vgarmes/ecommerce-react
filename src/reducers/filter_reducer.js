9178d11b8b9afd0a71bf7ff5050093351f58b0d0
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-05 13:07:55 +0200

    added dispatchers for setting the grid view

---
+++ src/reducers/filter_reducer.js
@@ -0,0 +1,38 @@
+import {
+  LOAD_PRODUCTS,
+  SET_LISTVIEW,
+  SET_GRIDVIEW,
+  UPDATE_SORT,
+  SORT_PRODUCTS,
+  UPDATE_FILTERS,
+  FILTER_PRODUCTS,
+  CLEAR_FILTERS,
+} from "../actions";
+
+const filter_reducer = (state, action) => {
+  if (action.type === LOAD_PRODUCTS) {
+    return {
+      ...state,
+      all_products: [...action.payload], // payload has to be spread so that we make a copy of it and not point to it
+      filtered_products: [...action.payload],
+    };
+  }
+
+  if (action.type === SET_GRIDVIEW) {
+    return {
+      ...state,
+      grid_view: true,
+    };
+  }
+
+  if (action.type === SET_LISTVIEW) {
+    return {
+      ...state,
+      grid_view: false,
+    };
+  }
+
+  throw new Error(`No matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;

c41a040c8c29ffba5728a4f03a9afd36a91788c9
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-05 13:06:41 +0200

    added main components

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { PageBreadcrumbs, Filters, Sort, ProductList } from "../components";
+import {
+  Container,
+  Button,
+  Grid,
+  Typography,
+  Box,
+  Paper,
+} from "@material-ui/core";
 
 const ProductsPage = () => {
-  return <div>Products Page</div>;
+  return (
+    <main>
+      <PageBreadcrumbs title="Products" />
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item xs={3}>
+            <Filters />
+          </Grid>
+
+          <Grid item xs={9}>
+            <Sort />
+            <ProductList />
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
+  );
 };
 
 export default ProductsPage;

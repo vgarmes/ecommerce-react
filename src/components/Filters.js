b2c6682e6c89e43d6f621d5c9da0b55877b8739f
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-05 23:58:28 +0200

    styled all filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -9,8 +9,13 @@ import {
   IconButton,
   Typography,
   Button,
+  FormControl,
+  InputLabel,
+  Select,
+  Slider,
 } from "@material-ui/core";
-import SearchIcon from "@material-ui/icons/Search";
+import styled from "styled-components";
+import { Search as SearchIcon, Check as CheckIcon } from "@material-ui/icons";
 
 const Filters = () => {
   const classes = useStyles();
@@ -49,7 +54,7 @@ const Filters = () => {
             <SearchIcon />
           </IconButton>
         </Box>
-        <Box className="form-control">
+        <Box className={classes.formControl}>
           <Typography
             variant="body1"
             className={classes.categoryTitle}
@@ -71,7 +76,70 @@ const Filters = () => {
             </Button>
           ))}
         </Box>
-        <Box className="form-control"></Box>
+        <Box className={classes.formControl}>
+          <Typography
+            variant="body1"
+            className={classes.categoryTitle}
+            gutterBottom
+          >
+            Brand
+          </Typography>
+          <Select
+            native
+            value={brand}
+            inputProps={{
+              name: "brand",
+              id: "brand",
+            }}
+            className={classes.selectInput}
+          >
+            {brands.map((singleBrand, index) => (
+              <option key={index} value={singleBrand}>
+                {singleBrand}
+              </option>
+            ))}
+          </Select>
+        </Box>
+        <Box className={classes.formControl}>
+          <Typography
+            variant="body1"
+            className={classes.categoryTitle}
+            gutterBottom
+          >
+            Colors
+          </Typography>
+          <Box display="flex" alignItems="center">
+            {colors.map((c, index) => {
+              if (c === "all") {
+                return <Button key={index}>All</Button>;
+              }
+              return (
+                <ColorButton key={index} color={c}>
+                  {c === color ? <CheckIcon /> : null}
+                </ColorButton>
+              );
+            })}
+          </Box>
+        </Box>
+        <Box clasName={classes.formControl}>
+          <Typography
+            variant="body1"
+            className={classes.categoryTitle}
+            gutterBottom
+          >
+            Price range
+          </Typography>
+          <Slider
+            value={[min_price, max_price]}
+            onChange={console.log("slider")}
+            valueLabelDisplay="auto"
+            aria-labelledby="range-slider"
+            getAriaValueText={formatPrice}
+          />
+        </Box>
+        <Button variant="contained" color="primary">
+          clear filters
+        </Button>
       </Paper>
     </div>
   );
@@ -85,7 +153,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
   },
   formControl: {
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(4),
   },
   searchInput: {
     flex: 1,
@@ -107,6 +175,26 @@ const useStyles = makeStyles((theme) => ({
   active: {
     backgroundColor: theme.palette.action.selected,
   },
+  selectInput: {
+    width: "100%",
+  },
 }));
+
+const ColorButton = styled.button`
+  height: 1rem;
+  width: 1rem;
+  border-radius: 50%;
+  background: ${(props) => (props.color ? props.color : "#222")};
+  margin-left: 0.5rem;
+  border: ${(props) => (props.color === "#FFFFFF" ? "1px solid #222" : "none")};
+  cursor: pointer;
+  opacity: ${(props) => (props.isActive ? "1" : "0.5")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  svg {
+    color: #fff;
+  }
+`;
 
 export default Filters;

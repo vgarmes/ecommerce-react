6c234e38cf5ed0024234561ccd938586befda685
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-07 11:52:45 +0200

    created starter files for cart context

---
+++ src/context/cart_context.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
+import {
+  ADD_TO_CART,
+  REMOVE_CART_ITEM,
+  TOGGLE_CART_ITEM_AMOUNT,
+  CLEAR_CART,
+  COUNT_CART_TOTALS,
+} from "../actions";
---
+++ src/reducers/cart_reducer.js
@@ -0,0 +1,7 @@
+import {
+  ADD_TO_CART,
+  CLEAR_CART,
+  COUNT_CART_TOTALS,
+  REMOVE_CART_ITEM,
+  TOGGLE_CART_ITEM_AMOUNT,
+} from "../actions";

d8d244c8a173716edd5cde4266eb5d36573d5f3c
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-07 14:25:02 +0200

    basic cart page finished

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import { CartContent, PageBreadcrumbs } from "../components";
+import { Container, Box, Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const CartPage = () => {
-  return <div>Cart Page</div>;
+  const classes = useStyles();
+  const { cart } = useCartContext();
+  if (cart.length < 1) {
+    return (
+      <Container component="main">
+        <PageBreadcrumbs title="Cart" />
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          className={classes.empty}
+        >
+          <Typography variant="h2" gutterBottom>
+            Your cart is empty
+          </Typography>
+          <Button
+            component={Link}
+            to="/products"
+            variant="contained"
+            color="primary"
+          >
+            Fill it
+          </Button>
+        </Box>
+      </Container>
+    );
+  }
+  return (
+    <Container component="main">
+      <PageBreadcrumbs title="Cart" />
+      <CartContent />
+    </Container>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  empty: {
+    minHeight: "80vh",
+  },
+}));
 
 export default CartPage;

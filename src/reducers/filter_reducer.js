bea252c3e98a4ee6bc7e3fbf2462a12af4692681
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-07 10:01:41 +0200

    finished filtering functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -73,7 +73,32 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    return { ...state };
+    const { all_products } = state;
+    const { text, category, brand, color, price } = state.filters;
+
+    let tempProducts = [...all_products];
+
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.model.toLowerCase().startsWith(text);
+      });
+    }
+
+    if (category !== "all") {
+      tempProducts = tempProducts.filter(
+        (product) => product.category === category
+      );
+    }
+
+    if (color !== "all") {
+      tempProducts = tempProducts.filter((product) => {
+        return product.variants.find((variant) => variant.hex === color);
+      });
+    }
+
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    return { ...state, filtered_products: tempProducts };
   }
 
   if (action.type === CLEAR_FILTERS) {

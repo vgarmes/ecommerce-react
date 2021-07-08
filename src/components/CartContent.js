20b42a98768b80ab6ef4306646a88b3f63857944
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-08 23:28:51 +0200

    added responsiveness to buttons

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -4,38 +4,45 @@ import { Link } from "react-router-dom";
 import CartTable from "./CartTable";
 import CartItem from "./CartItem";
 import CartTotals from "./CartTotals";
-import { Paper, Box, Divider, Button } from "@material-ui/core";
+import { Paper, Box, Divider, Button, useMediaQuery } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const CartContent = () => {
   const { cart, clearCart } = useCartContext();
   const classes = useStyles();
+
   return (
-    <Box className={classes.content}>
+    <Box mt={4} mb={4}>
       <CartTable cart={cart} />
-      <div>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mb={2}
+        flexDirection={useMediaQuery("(min-width:400px)") ? "row" : "column"}
+      >
         <Button
           component={Link}
           to="/products"
           variant="contained"
           color="primary"
+          style={{
+            marginBottom: `${
+              useMediaQuery("(min-width:400px)") ? "0" : "10px"
+            }`,
+          }}
         >
           Continue shopping
         </Button>
         <Button variant="contained" color="secondary" onClick={clearCart}>
           clear cart
         </Button>
-      </div>
+      </Box>
       <CartTotals />
     </Box>
   );
 };
 
-const useStyles = makeStyles((theme) => ({
-  content: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-  },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 export default CartContent;

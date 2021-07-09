818512c36b2ef3aad52f94e4906b299a827cf23f
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-09 10:49:42 +0200

    user context takes variables from Auth0 and provides them to app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { StylesProvider, ThemeProvider } from "@material-ui/core/styles";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 import theme from "./theme";
 import "./index.css";
@@ -17,16 +18,18 @@ ReactDOM.render(
     redirectUri={window.location.origin}
     cacheLocation="localstorage"
   >
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-          </ThemeProvider>
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <App />
+            </ThemeProvider>
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
   </Auth0Provider>,
   document.getElementById("root")
 );

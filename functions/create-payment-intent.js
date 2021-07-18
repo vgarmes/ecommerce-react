383883700e49f268cd5e526b86023b8cd2f825dd
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-18 15:22:30 +0200

    finished payment intent function

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -1,13 +1,31 @@
 // domain/.netlify/functions/create-payment-intent
+require("dotenv").config(); //require dotenv package and invoke env variables
+
+const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 exports.handler = async function (event, context) {
   if (event.body) {
     const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
-    console.log(cart);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(cart),
-    };
+
+    const calculateOrderAmount = () => {
+      return shipping_fee + total_amount;
+    }; // for increased security, total amount should be recalculated by iterating over the items of the cart
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: "usd",
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ msg: error.message }),
+      };
+    }
   }
   return {
     statusCode: 200,

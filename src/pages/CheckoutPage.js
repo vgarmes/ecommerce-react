8a259c6ed695f94a92905065db28bd6ed1d30901
Victor Garcia Mestre <vgmestre@gmail.com>
2021-07-31 16:46:45 +0200

    Confirmation page is imported as an external component

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -13,7 +13,9 @@ import { PageBreadcrumbs, CartEmpty } from "../components";
 import StripeCheckout from "../components/StripeCheckout";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AdressForm from "../components/Checkout/AdressForm";
+import Confirmation from "../components/Checkout/Confirmation";
 import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
 
 const steps = ["Shipping address", "Payment details", "Confirmation"];
 
@@ -21,7 +23,7 @@ const CheckoutPage = () => {
   const classes = useStyles();
   const theme = useTheme();
   const { cart } = useCartContext();
-  const [activeStep, setActiveStep] = useState(0);
+  const [activeStep, setActiveStep] = useState(2);
   const smallScreen = useMediaQuery(theme.breakpoints.down("xs"));
 
   const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -31,7 +33,7 @@ const CheckoutPage = () => {
     if (activeStep === 0) {
       return (
         <>
-          <AdressForm secondaryAction={nextStep} />
+          <AdressForm onSuccessAction={nextStep} />
           <Box display="flex" justifyContent="flex-end" mt={4} mb={2}>
             <Button
               form="address-form"
@@ -47,27 +49,29 @@ const CheckoutPage = () => {
     } else if (activeStep === 1) {
       return (
         <>
-          <StripeCheckout />
+          <StripeCheckout onSuccessAction={nextStep} />
           <Box display="flex" justifyContent="space-between" mt={4} mb={2}>
             <Button variant="contained" color="secondary" onClick={backStep}>
               Previous
-            </Button>
-            <Button variant="contained" color="primary" onClick={nextStep}>
-              Next
             </Button>
           </Box>
         </>
       );
     } else {
-      return <Confirmation />;
+      return (
+        <>
+          <Confirmation />
+          <Box display="flex" justifyContent="center">
+            <Button component={Link} to="/" variant="contained" color="primary">
+              Main menu
+            </Button>
+          </Box>
+        </>
+      );
     }
   };
 
-  const Confirmation = () => {
-    return <div>Confirmation</div>;
-  };
-
-  if (cart.length < 1) {
+  if (cart.length < 1 && activeStep === 0) {
     return <CartEmpty title="Checkout" />;
   }
 
@@ -92,13 +96,16 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(4),
-    padding: theme.spacing(2),
+    padding: theme.spacing(3),
+    [theme.breakpoints.up("md")]: {
+      padding: theme.spacing(3, 6),
+    },
     width: "100%",
     maxWidth: "900px",
     margin: "auto",
   },
   stepper: {
-    padding: theme.spacing(3, 0, 5),
+    padding: theme.spacing(1, 0, 5),
     [theme.breakpoints.up("md")]: {
       padding: theme.spacing(3, 10, 5),
     },
